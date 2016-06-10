0634accae28ab091083c99d03b2d411e53350fd8
Charlestown <cdcunha@gmail.com>
2016-06-10 10:08:24 -0300

    Acrescentado os Fontes

---
+++ ViewGenericFile/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace InterfaceColWeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonConfigFile = new System.Windows.Forms.Button();
+            this.labelXMLConfigFile = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.textBoxGotoLine = new System.Windows.Forms.TextBox();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelQtdeRegistros = new System.Windows.Forms.Label();
+            this.buttonGotoLine = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "XML";
+            this.openFileDialog1.Filter = "Arquivo XML (.xml)|*.xml";
+            // 
+            // buttonConfigFile
+            // 
+            this.buttonConfigFile.Location = new System.Drawing.Point(12, 17);
+            this.buttonConfigFile.Name = "buttonConfigFile";
+            this.buttonConfigFile.Size = new System.Drawing.Size(110, 23);
+            this.buttonConfigFile.TabIndex = 0;
+            this.buttonConfigFile.Text = "Abrir mapeamento";
+            this.toolTip1.SetToolTip(this.buttonConfigFile, resources.GetString("buttonConfigFile.ToolTip"));
+            this.buttonConfigFile.UseVisualStyleBackColor = true;
+            this.buttonConfigFile.Click += new System.EventHandler(this.buttonConfigFile_Click);
+            // 
+            // labelXMLConfigFile
+            // 
+            this.labelXMLConfigFile.AutoSize = true;
+            this.labelXMLConfigFile.Location = new System.Drawing.Point(128, 22);
+            this.labelXMLConfigFile.Name = "labelXMLConfigFile";
+            this.labelXMLConfigFile.Size = new System.Drawing.Size(0, 13);
+            this.labelXMLConfigFile.TabIndex = 1;
+            // 
+            // textBoxGotoLine
+            // 
+            this.textBoxGotoLine.Location = new System.Drawing.Point(222, 49);
+            this.textBoxGotoLine.Name = "textBoxGotoLine";
+            this.textBoxGotoLine.Size = new System.Drawing.Size(61, 20);
+            this.textBoxGotoLine.TabIndex = 6;
+            this.toolTip1.SetToolTip(this.textBoxGotoLine, "Informe a linha desejada");
+            this.textBoxGotoLine.Click += new System.EventHandler(this.buttonGotoLine_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(13, 47);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(109, 23);
+            this.buttonOpenFile.TabIndex = 2;
+            this.buttonOpenFile.Text = "Abrir arquivo texto";
+            this.toolTip1.SetToolTip(this.buttonOpenFile, "Abrir o arquivo texto");
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 76);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(754, 266);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.dataGridView1_KeyUp);
+            // 
+            // labelQtdeRegistros
+            // 
+            this.labelQtdeRegistros.AutoSize = true;
+            this.labelQtdeRegistros.Location = new System.Drawing.Point(128, 52);
+            this.labelQtdeRegistros.Name = "labelQtdeRegistros";
+            this.labelQtdeRegistros.Size = new System.Drawing.Size(0, 13);
+            this.labelQtdeRegistros.TabIndex = 4;
+            this.labelQtdeRegistros.TextChanged += new System.EventHandler(this.labelQtdeRegistros_TextChanged);
+            // 
+            // buttonGotoLine
+            // 
+            this.buttonGotoLine.Location = new System.Drawing.Point(289, 47);
+            this.buttonGotoLine.Name = "buttonGotoLine";
+            this.buttonGotoLine.Size = new System.Drawing.Size(75, 23);
+            this.buttonGotoLine.TabIndex = 5;
+            this.buttonGotoLine.Text = "Ir para linha";
+            this.buttonGotoLine.UseVisualStyleBackColor = true;
+            this.buttonGotoLine.Click += new System.EventHandler(this.buttonGotoLine_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 354);
+            this.Controls.Add(this.textBoxGotoLine);
+            this.Controls.Add(this.buttonGotoLine);
+            this.Controls.Add(this.labelQtdeRegistros);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.labelXMLConfigFile);
+            this.Controls.Add(this.buttonConfigFile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arquivos de Interface do ColWeb";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonConfigFile;
+        private System.Windows.Forms.Label labelXMLConfigFile;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelQtdeRegistros;
+        private System.Windows.Forms.Button buttonGotoLine;
+        private System.Windows.Forms.TextBox textBoxGotoLine;
+        private System.Data.DataSet dataSet1;
+    }
+}
+
---
+++ ViewGenericFile/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using GenericParsing;
+
+namespace InterfaceColWeb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            buttonOpenFile.Visible = false;
+            textBoxGotoLine.Visible = false;
+            buttonGotoLine.Visible = false;
+        }
+
+        private int convertLabelQtdRegToInt()
+        {
+            return Convert.ToInt32(labelQtdeRegistros.Text.Replace(" Registros", "").Replace(" Registro", "").Replace(".", ""));
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(dataGridView1.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, b, e.RowBounds.Location.X + 10, e.RowBounds.Location.Y + 4);
+            }
+        }
+        
+        private void buttonConfigFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "Arquivo XML (.xml)|*.xml";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                labelXMLConfigFile.Text = openFileDialog1.FileName;
+                buttonOpenFile.Visible = true;
+            }
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Application.StartupPath;
+            openFileDialog1.Filter = "Todos os arquivos (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {   
+                if (!string.IsNullOrEmpty(labelXMLConfigFile.Text))
+                {
+                    dataGridView1.DataSource = null;
+                    using (GenericParserAdapter parser = new GenericParserAdapter(openFileDialog1.FileName))
+                    {
+                        parser.Load(labelXMLConfigFile.Text);
+                        dataSet1 = parser.GetDataSet();
+
+                        int qtdRegistros = dataSet1.Tables[0].Rows.Count;
+                        if (qtdRegistros > 1)
+                        {
+                            labelQtdeRegistros.Text = String.Format("{0:#,##0} Registros", dataSet1.Tables[0].Rows.Count);
+                        }
+                        else
+                        {
+                            labelQtdeRegistros.Text = String.Format("{0:#,##0} Registro", dataSet1.Tables[0].Rows.Count);
+                        }
+
+                        dataGridView1.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCellsExceptHeader);
+                        dataGridView1.AutoGenerateColumns = true;
+                        dataGridView1.DataSource = dataSet1.Tables[0];
+                    }
+                }
+            }
+        }
+
+        private void buttonGotoLine_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxGotoLine.Text))
+            {
+                try
+                {
+                    int numLin = Convert.ToInt32(textBoxGotoLine.Text);
+                    int qtdReg = convertLabelQtdRegToInt();
+                    if (numLin <= qtdReg)
+                    {
+                        numLin--;
+                        dataGridView1.Rows[numLin].Selected = true;
+                        dataGridView1.FirstDisplayedScrollingRowIndex = numLin;
+                    }
+                    else
+                    {
+                        MessageBox.Show(String.Format("O número da linha desejada deve ser menor ou igual à {0}", qtdReg));
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Número inválido");
+                }
+            }
+        }
+
+        private void labelQtdeRegistros_TextChanged(object sender, EventArgs e)
+        {
+            if (convertLabelQtdRegToInt() > 0)
+            {
+                textBoxGotoLine.Visible = true;
+                buttonGotoLine.Visible = true;
+            }
+            else
+            {
+                textBoxGotoLine.Visible = false;
+                buttonGotoLine.Visible = false;
+            }
+        }
+
+        private void dataGridView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up: moveUp(); break;
+                case Keys.Down: moveDown(); break;
+                case Keys.PageUp: movePageUp(); break;
+                case Keys.PageDown: movePageDown(); break;
+                default: break;
+            }
+            e.Handled = true;
+        }
+
+        private void movimentGrid(int lines)
+        {
+            if (lines != 0) {
+                if (dataGridView1.RowCount > 0)
+                {
+                    int rowCount = dataGridView1.Rows.Count;
+                    if (lines > 0)
+                    {
+                        if (dataGridView1.FirstDisplayedScrollingRowIndex + lines >= dataGridView1.Rows.Count)
+                        {
+                            dataGridView1.FirstDisplayedScrollingRowIndex = dataGridView1.Rows.Count -1;
+                        }
+                        else
+                        {
+                            dataGridView1.FirstDisplayedScrollingRowIndex += lines;
+                        }
+                    }
+                    else
+                    {
+                        if (dataGridView1.FirstDisplayedScrollingRowIndex - lines <= 0)
+                        {
+                            dataGridView1.FirstDisplayedScrollingRowIndex = 0;
+                        }
+                        else
+                        {
+                            dataGridView1.FirstDisplayedScrollingRowIndex -= lines;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void moveUp()
+        {
+            movimentGrid(-1);
+        }
+
+        private void moveDown()
+        {
+            movimentGrid(1);
+        }
+
+        private void movePageUp()
+        {
+            movimentGrid(-5);
+        }
+
+        private void movePageDown()
+        {
+            movimentGrid(5);
+        }
+    }
+}
